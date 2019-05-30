4c30cccc982b13d38e3c9b3e2c375e08256a7112
filipe costa <lfilipe82@yahoo.com.br>
2019-05-30 16:42:14 -0300

    controller view routes for cocktails ok

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to doses_path(@dose)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to doses_path(@dose)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
